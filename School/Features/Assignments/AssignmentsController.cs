63aed35a84164788a9e5a43aa28a848ca4351979
Moata Alexandru <alexandru.moata@student.upt.ro>
2022-12-16 20:22:10 +0200

    Workshop1 - Post,Get,Get{id}

---
+++ School/Features/Assignments/AssignmentsController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using School.Features.Assignments.Models;
+using School.Features.Assignments.Views;
+
+namespace School.Features.Assignments;
+
+[ApiController]
+[Route("assignments")]
+public class AssignmentsController
+{
+    private static List<AssignmentModel> _mockDb = new List<AssignmentModel>(); //Nu avem baza de date, deci folosim o list pe post de DB de asta avem mockDb
+
+    public AssignmentsController() { }
+
+    [HttpPost] //Tag-ul adauga informatii in baza de date
+    public AssignmentsResponse Add(AssignmentsRequest request)
+    {
+        var assignment = new AssignmentModel
+        {
+            Id = Guid.NewGuid().ToString(),
+            Created = DateTime.UtcNow,
+            Updated = DateTime.UtcNow,
+            Subject = request.Subject,
+            Description = request.Description,
+            Deadline = request.Deadline
+        };
+        
+        _mockDb.Add(assignment);
+
+        return new AssignmentsResponse
+        {
+            Id = assignment.Id,
+            Subject = assignment.Subject,
+            Description = assignment.Description,
+            Deadline = assignment.Deadline
+        };
+    }
+
+    [HttpGet]
+    public IEnumerable<AssignmentsResponse> Get()
+    {
+        return _mockDb.Select(
+            assignment => new AssignmentsResponse
+            {
+                Id = assignment.Id,
+                Subject = assignment.Subject,
+                Description = assignment.Description,
+                Deadline = assignment.Deadline
+            }).ToList();
+    }
+
+    [HttpGet("{id}")]
+    public AssignmentsResponse Get([FromRoute] string id)
+    {
+        var assignment = _mockDb.FirstOrDefault(x => x.Id == id);
+        if (assignment is null)
+        {
+            return null;
+        }
+        return new AssignmentsResponse
+        {
+            Id = assignment.Id,
+            Subject = assignment.Subject,
+            Description = assignment.Description,
+            Deadline = assignment.Deadline
+        };
+    }
+    //[HttpDelete]
+    
+    
+    //[HttpPatch]
+}
---
+++ School/Features/Assignments/Views/AssignmentsResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace School.Features.Assignments.Views;
+
+public class AssignmentsResponse
+{
+    public string Id { get; set; }
+    public string Subject { get; set; }
+    public string Description { get; set; }
+    public DateTime Deadline { get; set; }
+}

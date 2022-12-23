76b88134b40928d36724584fab153a5a6fadde4c
Moata Alexandru <alexandru.moata@student.upt.ro>
2022-12-23 16:39:53 +0200

    Ultimele detalii Test

--- School/Features/Test/TestController.cs
+++ School/Features/Test/TestController.cs
@@ -31,4 +31,76 @@ public class TestController
             TestDate = test.TestDate
         };
     }
+    
+    [HttpGet]
+    public IEnumerable<TestResponse> Get()
+    {
+        return _mockDb.Select(
+            assignment => new TestResponse
+            {
+                Id = assignment.Id,
+                Subject = assignment.Subject,
+                TestDate = assignment.TestDate
+            }).ToList();
+    }
+    
+    [HttpGet("{id}")]
+    public TestResponse Get([FromRoute] string id)
+    {
+        var assignment = _mockDb.FirstOrDefault(x => x.Id == id);
+        if (assignment is null)
+        {
+            return null;
+        }
+        return new TestResponse
+        {
+            Id = assignment.Id,
+            Subject = assignment.Subject,
+            TestDate = assignment.TestDate
+        };
+    }
+    
+    [HttpDelete]
+    
+    public TestResponse Delete([FromRoute] string id)
+    {
+        var assignment = _mockDb.FirstOrDefault(x => x.Id == id);
+
+        if (assignment is null)
+        {
+            return null;
+        }
+        
+        _mockDb.Remove(assignment);
+        
+        return new TestResponse
+        {
+            Id = assignment.Id,
+            Subject = assignment.Subject,
+            TestDate = assignment.TestDate
+        };
+    }
+    
+    [HttpPatch("{id}")]
+    public TestResponse Update([FromRoute] string id,[FromBody] TestRequest request)
+    {
+        var assignment = _mockDb.FirstOrDefault(user => user.Id == id);
+        if (assignment is null)
+        {
+            return null;
+            
+        }
+
+        assignment.Updated = DateTime.UtcNow;
+        assignment.Subject = request.Subject;
+        assignment.TestDate = request.TestDate
+
+        return new TestResponse
+        {
+            Id = assignment.Id,
+            Subject = assignment.Subject,
+            TestDate = assignment.TestDate
+        };
+    }
+    
 }
--- School/Features/Test/ViewsT/TestRequest.cs
+++ School/Features/Test/ViewsT/TestRequest.cs
@@ -5,6 +5,5 @@ namespace School.Features.Test.ViewsT;
 public class TestRequest
 {
     [Required]public string Subject { get; set; }
-
     [Required]public DateTime TestDate { get; set; }
 }
